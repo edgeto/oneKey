ee8072724779c760745debddeb6f8ff7858a0616
qiuqiuyuan <1511732855@qq.com>
2017-07-12 17:34:12 +0800

    onekey

--- php_extend.sh
+++ php_extend.sh
@@ -9,6 +9,11 @@ php_ini_path="/usr/local/php/lib/php.ini"
 igbinary_tar_gz="igbinary-2.0.1.tgz" 
 igbinary_dir="igbinary-2.0.1" 
 igbinary_name="igbinary" 
+redis_server_tar_gz="redis-3.0.7.tar.gz" 
+redis_server_dir="redis-3.0.7" 
+redis_server_name="redis" 
+redis_server_path="/usr/local/redis"
+local_pwd=`pwd`
 redis_tar_gz="redis-2.2.7.tgz" 
 redis_dir="redis-2.2.7" 
 redis_name="redis" 
@@ -42,6 +47,33 @@ then
 else
 	echo "${date_show_str} ----- 请先上传${igbinary_tar_gz}源码 -----"
 	echo "${date_show_str} ----- 请先上传${igbinary_tar_gz} -----" >> ${server_install_log_path}
+fi
+
+##### redis_server安装
+if [ -e "${redis_server_tar_gz}" ]
+then
+	echo "${date_show_str} ----- ${redis_server_tar_gz}源码文件存在 -----"
+	echo "${date_show_str} ----- ${redis_server_tar_gz}源码文件存在 -----" >> ${server_install_log_path}
+	echo "${date_show_str} ----- 解压${redis_server_tar_gz} -----"
+	echo "${date_show_str} ----- 解压${redis_server_tar_gz} -----" >> ${server_install_log_path}
+	tar zxvf ${redis_server_tar_gz}
+	cp -r ${redis_server_dir} ${redis_server_path} 
+	cd ${redis_server_path}
+	make && make install
+	cp redis.conf /etc/
+	##### 将redis添加到自启动中  
+	echo "/usr/local/bin/redis-server /etc/redis.conf" >> /etc/rc.d/rc.local
+	##### 启动redis  
+	redis-server /etc/redis.conf
+	echo "${date_show_str} ----- 成功安装redis_server -----"
+	echo "${date_show_str} ----- 成功安装redis_server -----" >> ${server_install_log_path}
+	##### 返回上一目录
+	echo "${date_show_str} ----- 返回上一目录 -----"
+	echo "${date_show_str} ----- 返回上一目录 -----" >> ${server_install_log_path}
+	cd ${local_pwd}
+else
+	echo "${date_show_str} ----- 请先上传${redis_server_tar_gz}源码 -----"
+	echo "${date_show_str} ----- 请先上传${redis_server_tar_gz} -----" >> ${server_install_log_path}
 fi
 
 ##### redis安装
